ed68a73d8fb25012b0b83f70ad89faf09944b9f0
Chris Ochs <Chris Ochs@10.0.0.160>
2017-05-27 17:47:56 -0400

    Added Target AbN Derivation Explanation

--- src/main/java/edu/njit/cs/saboc/blu/owl/gui/gep/panels/range/OWLRangeAbNOptionsPanel.java
+++ src/main/java/edu/njit/cs/saboc/blu/owl/gui/gep/panels/range/OWLRangeAbNOptionsPanel.java
@@ -2,10 +2,10 @@ package edu.njit.cs.saboc.blu.owl.gui.gep.panels.range;
 
 import edu.njit.cs.saboc.blu.core.abn.targetbased.TargetAbstractionNetwork;
 import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.AbNOptionsPanel;
-import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.optionbuttons.abn.AbNHelpButton;
 import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.optionbuttons.abn.ExportAbNButton;
 import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.optionbuttons.abn.PopoutAbNDetailsButton;
 import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.optionbuttons.abn.SavePNGButton;
+import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.targetabn.buttons.TargetAbNHelpButton;
 import edu.njit.cs.saboc.blu.owl.gui.gep.panels.range.configuration.OWLRangeAbNConfiguration;
 
 /**
@@ -15,7 +15,8 @@ import edu.njit.cs.saboc.blu.owl.gui.gep.panels.range.configuration.OWLRangeAbNC
 public class OWLRangeAbNOptionsPanel extends AbNOptionsPanel<TargetAbstractionNetwork> {
     
     public OWLRangeAbNOptionsPanel(OWLRangeAbNConfiguration config) {
-        AbNHelpButton helpBtn = new AbNHelpButton(config);
+        
+        TargetAbNHelpButton helpBtn = new TargetAbNHelpButton(config);
         ExportAbNButton exportBtn = new ExportAbNButton("Export Range Abstraction Network", config);
         SavePNGButton pngBtn = new SavePNGButton(config);
         PopoutAbNDetailsButton popoutBtn = new PopoutAbNDetailsButton(config);
